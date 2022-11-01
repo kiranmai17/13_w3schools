199095096aa4739e478e71c6b883308cfb6c22ab
kiranmai17 <116304133+kiranmai17@users.noreply.github.com>
2022-11-01 15:10:59 +0530

    Add files via upload

---
+++ W3schools.py
@@ -0,0 +1,21 @@
+
+'''DATE=1st nov 2022
+DAY= monday
+TOPIC= W3 solving
+AUTHOR= kiranmai'''
+
+x=['hello','kiran']
+print(type(x)) #list
+x=[]
+print(type(x))
+x={'hello':'pooja'}
+print(type(x))  #dict
+x={}
+print(type(x))  #dict
+y={3}
+print(type(y)) #set
+y={'.'}
+print(type(y)) #set
+x=6
+x=complex(x)
+print(x)   #6+0j
