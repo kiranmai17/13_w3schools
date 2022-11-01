dded9ec6a3b283a80077be7e385993ed6a31cf7f
kiranmai17 <116304133+kiranmai17@users.noreply.github.com>
2022-11-01 15:25:40 +0530

    Add files via upload

---
+++ w3practise.py
@@ -0,0 +1,8 @@
+x='hey kiran'
+print(x[4]) #k
+print(len(x)) #9
+print(x.replace('k','H')) #hey Hiran
+print(bool('kiran')) #true
+print(bool('')) #false
+print(bool(0)) #false
+print(bool('.'))#true
